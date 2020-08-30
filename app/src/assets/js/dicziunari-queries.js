7e80daddc193be3109410a31282a02981fcbfea2
Gion-Andri Cantieni <gion-andri@gmx.ch>
2020-08-30 08:44:22 +0200

    fix: update queries

--- app/src/assets/js/dicziunari-queries.js
+++ app/src/assets/js/dicziunari-queries.js
@@ -4,18 +4,18 @@ var sqlQueries = {
     getCountSqlQuery: function () {
         return '' +
             'select count(1) as myCount ' +
-            'from dictionary_idx ' +
-            'where dictionary_idx match ? ';
+            'from rumgr_idx ' +
+            'where rumgr_idx match ? ';
     },
 
     getItemsSqlQuery: function () {
         var query = '' +
-            'select c.original, c.translation, ' +
+            'select c.lemma, c.translation, ' +
             'c.direction, c.weight ' +
-            'from dictionary_content c, dictionary_idx idx ' +
-            'where idx.dictionary_idx match ? ' +
-            'and idx.rowid = c.id ' + 
-            'order by c.weight, length(translation), c.line_csv ' +
+            'from rumgr c, rumgr_idx idx ' +
+            'where rumgr_idx match ? ' +
+            'and idx.id = c.id ' +
+            'order by c.weight, length(translation) ' +
             'limit ? offset ?';
         return query;
     },
@@ -23,7 +23,7 @@ var sqlQueries = {
     getFtsQuery: function (query) {
         var escapedQuery = query.replace(/"/g, "");
         escapedQuery = escapedQuery.split('*').join('"*"');
-        return 'keywords: "' + escapedQuery + '"';
+        return 'keyword: "' + escapedQuery + '"';
     }
 };
 
